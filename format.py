1cdc7ff8eb202f880967491176ae30801848e02a
longsail <yuanhang.nju@gmail.com>
2014-05-22 15:41:50 +0800

    comput word in-dgree out-dgree in function get_wordgree

--- format.py
+++ format.py
@@ -1,5 +1,6 @@
 #coding=utf-8
 import jieba
+from collections import defaultdict
 from collections import Counter
 
 def get_stopwords(filename='stopwords'):
@@ -30,20 +31,33 @@ def format(filename='data.txt'):
             if string_to_file:
                 f.write(string_to_file+'\n')
 
-def get_wordgraph(filename='reply.txt',window_size=4):
+def get_worddgree(filename='reply.txt',window_size=4):
     with open(filename,'r') as f:
         word_seq = f.read()
 
     seg_list = jieba.cut(word_seq)
-    seg_list = [word.encode('utf-8') for word in seg_list]
+    word_list = [word.encode('utf-8') for word in seg_list]
     length = len(seg_list)
-    word_graph = [seg_list[i]+' '+seg_list[i+j] for i in xrange(0,length-window_size) for j in xrange(1,window_size)]
-    word_graph_dict = Counter(word_graph)
+    wordgraph = [word_list[i]+' '+word_list[i+j] for i in xrange(0,length-window_size) for j in xrange(1,window_size)]
+    wordgraph_dict = Counter(word_graph)
+    #word_graph_set = set(word_graph)
+    word_indgree = defaultdict(lambda:defaultdict(int))
+    word_outdgree = defaultdict(int)
+    for edge in word_graph_dict:
+	words = edge.split(' ')
+	word_i = words[0]
+	word_j = words[1]
+	word_indgree[word_j][word_i]= wordgraph_dict[edge]
+	word_outdgree[word_i] += wordgraph_dict[edge]
+    return word_indgree,word_outdgree
 
-    with open('word_graph.txt','w') as f:
-        for edge in word_graph_dict:
-            weight = str(word_graph_dict[edge])
-            f.write(edge+' '+weight+'\n')
+
+    
+
+    #with open('word_graph.txt','w') as f:
+    #    for edge in word_graph_dict:
+    #        weight = str(word_graph_dict[edge])
+    #        f.write(edge+' '+weight+'\n')
 
 if __name__ == "__main__":
     #format()
